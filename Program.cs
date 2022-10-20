1df804b8647d12d533cea4beaeaae9e603e386f6
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2022-10-20 19:27:11 -0300

    Stack exemple

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+Stack<int> pilhas = new Stack<int>();
+
+//Adicionando itens na stack
+pilhas.Push(2);
+pilhas.Push(4);
+pilhas.Push(6);
+pilhas.Push(8);
+
+foreach(int item in pilhas)
+{
+    Console.WriteLine(item);
+}
+
+var remove = pilhas.Pop(); //remove o último elemento que entrou na pilha
+Console.WriteLine($"Removendo o item {remove}");
+
+foreach(int item in pilhas)
+{
+    Console.WriteLine(item);
+}
